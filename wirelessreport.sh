80a82b268bb660d820d58fb46591fe5564b8575b
JB1366 <jb8745@proton.me>
2026-06-15 15:49:12 -0500

    Wireless Report AiMesh

--- wirelessreport.sh
+++ wirelessreport.sh
@@ -272,7 +272,8 @@ do_update() {
     echo -e "\n${GR}[+] Downloading latest version (v$REMOTE_VER)${NC}"
     if curl -sfL --retry 3 "$GITHUB" -o "$REPORT_SCRIPT"; then
         chmod +x "$REPORT_SCRIPT" 2>/dev/null
-        return 0
+        sleep 3
+		return 0
     else
         echo -e "${RD}[!] Download failed. Sticking with current version.${NC}"
         return 1
