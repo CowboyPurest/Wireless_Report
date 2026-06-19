bbef054068320b38c557e4e43c71921872743a1d
JB1366 <jb8745@proton.me>
2026-06-18 21:49:05 -0500

    Wireless Report AiMesh

--- wirelessreport.sh
+++ wirelessreport.sh
@@ -1576,7 +1576,7 @@ get_row() {
 }
 
 final_chk() {
-	[ "${#ssid}" -eq 32 ] && ssid="BACKHAUL$NUMBERED_NODE"
+	
 	[ -z "$ssid" ] && ssid="Wireless"
     if [ "$rssi" -ge 0 ] && [ "$rssi" -le 1 ]; then
         rssi=-54
